a97da745373afeea4cd418b0dfdfc28580174d14
Michel <micheltt.dev@gmail.com>
2022-07-14 21:05:17 +0200

    Amazon clone init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="App">
+            <p className="px-20 text-primary">
+                Click on the Vite and React logos to learn more
+            </p>
+        </div>
+    );
+}
+
+export default App;
