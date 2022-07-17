bbd559bf332479d22900b84a7d634b6fdae5e82f
Michel <michel.tcha@gmail.com>
2022-07-17 17:22:57 +0200

    Link update, assets, img added

--- src/layout/Footer/FooterLinks.jsx
+++ src/layout/Footer/FooterLinks.jsx
@@ -5,58 +5,184 @@ const FooterLinks = () => {
                 <li className="text-white font-emberb text-base mb-2 whitespace-nowrap">
                     Pour mieux nous connaître
                 </li>
-                <li>À propos d'Amazon </li>
-                <li>Carrières </li>
-                <li>Durabilité </li>
-                <li>Amazon Science </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        À propos d'Amazon
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Carrières
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Durabilité
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Amazon Science
+                    </a>
+                </li>
             </ul>
             <ul className="flex flex-col gap-1 w-[180px]">
                 <li className="text-white font-emberb text-base mb-2 whitespace-nowrap">
                     Gagnez de l'argent
                 </li>
-                <li>Vendez sur Amazon</li>
-                <li>Vendez sur Amazon Business</li>
-                <li>Vendez sur Amazon Handmade</li>
-                <li>Vendez sur Amazon Launchpad</li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Vendez sur Amazon
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Vendez sur Amazon Business
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Vendez sur Amazon Handmade
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Vendez sur Amazon Launchpad
+                    </a>
+                </li>
 
-                <li>Devenez Partenaire </li>
-                <li>Expédié par Amazon </li>
-                <li>Faites la promotion de vos produits</li>
-                <li>Auto-publiez votre livre</li>
-                <li>Amazon Pay</li>
-                <li>› Voir plus : Gagner de l'argent avec nous</li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Devenez Partenaire
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Expédié par Amazon
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Faites la promotion de vos produits
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Auto-publiez votre livre
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Amazon Pay
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        › Voir plus : Gagner de l'argent avec nous
+                    </a>
+                </li>
             </ul>
 
             <ul className="flex flex-col gap-1 w-[180px]">
                 <li className="text-white font-emberb text-base mb-2 whitespace-nowrap">
                     Moyens de paiement Amazon
                 </li>
-                <li>Carte Amazon Business Amex</li>
-                <li>Cartes de paiement</li>
-                <li>Paiement en plusieurs fois</li>
-                <li>Amazon Currency Converter</li>
-                <li>Cartes cadeaux</li>
-                <li>Recharge en ligne</li>
-                <li>Recharge en point de vente</li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Carte Amazon Business Amex
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Cartes de paiement
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Paiement en plusieurs fois
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Amazon Currency Converter
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Cartes cadeaux
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Recharge en ligne
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Recharge en point de vente
+                    </a>
+                </li>
             </ul>
             <ul className="flex flex-col gap-1 w-[180px]">
                 <li className="text-white font-emberb text-base mb-2 whitespace-nowrap">
                     Besoin d'aide ?
                 </li>
-                <li>Amazon et COVID-19</li>
-                <li>Voir ou suivre vos commandes</li>
-                <li>Tarifs et options de livraison</li>
-                <li>Amazon Prime</li>
-                <li>Retours et remplacements</li>
                 <li>
-                    Recyclage (y compris les équipements électriques et
-                    électroniques)
+                    <a className="hover:underline" href="">
+                        Amazon et COVID-19
+                    </a>
                 </li>
-                <li>Infos sur notre Marketplace</li>
-                <li>Application Amazon Mobile</li>
-                <li>Amazon Assistant</li>
-                <li>Service Client</li>
-                <li>Accessibilité</li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Voir ou suivre vos commandes
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Tarifs et options de livraison
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Amazon Prime
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Retours et remplacements
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Recyclage (y compris les équipements électriques et
+                        électroniques)
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Infos sur notre Marketplace
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Application Amazon Mobile
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Amazon Assistant
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Service Client
+                    </a>
+                </li>
+                <li>
+                    <a className="hover:underline" href="">
+                        Accessibilité
+                    </a>
+                </li>
             </ul>
         </div>
     );
