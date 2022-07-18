41cf3e1adad7cb4cc5e6d7c5b57a58a91296a617
Michel Tcha <michel.tcha@gmail.com>
2022-07-18 13:35:14 +0200

    Update Card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,18 @@
+import modeImg from "../assets/img/theme/mode.jpg";
+
 const HomePage = () => {
-    return <main>This is the home page...</main>;
+    return (
+        <main className="w-full bg-dark py-20">
+            <section className="grid grid gap-8 bg-dark px-4">
+                <article className="min-w-20 w-48 p-5 bg-white border-black border-1">
+                    <h2 className="text-xl">Abonnement Amazon Prime</h2>
+                    <div className="py-2">
+                        <img src={modeImg} alt="mode" />
+                    </div>
+                    <span className="font-ember text-base">Découvrir</span>
+                </article>
+            </section>
+        </main>
+    );
 };
 export default HomePage;
