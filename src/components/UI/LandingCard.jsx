3c72752d66540963bfa06cf5269d9b987c769782
Michel <michel.tcha@gmail.com>
2022-07-23 12:11:23 +0200

    Testing Generate sourcemap

--- src/components/UI/LandingCard.jsx
+++ src/components/UI/LandingCard.jsx
@@ -1,7 +1,7 @@
 const LandingCard = ({ title, img, text }) => {
     return (
         <article className="h-[420px] min-w-[266px] flex flex-col gap-1 p-5 bg-white border-black border-1">
-            <h2 className="text-lg">{title}</h2>
+            <h4 className="text-lg">{title}</h4>
             <div className="h-[400px] flex-auto">
                 <img className="h-full w-full" src={img} alt="mode" />
             </div>
